f4f03c6288259e2360f74210aaf20c5a164d2fe5
Axel Sunem Mora Olvera <Axel Sunem Mora Olvera>
2017-02-18 13:57:55 -0700

    Comentarios

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # svm-master
-Shiny app que emplea SVM sobre datos de entrenamiento
-Esta es una sencilla shiny app que implementa SVM sobre un conjunto de 3000 datos creados por R y clasificados en 3 grupos. Del total de datos, especifique una submuestra, seleccione el tipo de kernel y en base a eso configure los parametros necesarios. Se muestra la tabla de confusion, el porcentaje correctamente clasificado segun la prediccion, el total de vectores soporte y la grafica.
-
+Esta es una sencilla shiny app que implementa SVM sobre un conjunto de datos.
+Por defecto, trabaja con un data frame de 3000 observaciones creados por R y clasificados en 3 clasess. 
+Puede optar por la opcion de usar un archivo CSV e importarlo en la aplicacion.
+Del total de datos, especifique una submuestra, seleccione el tipo de kernel y en base a eso configure los parametros necesarios. 
+Se muestra la tabla de confusion, el porcentaje correctamente clasificado segun la prediccion y el total de vectores soporte. En la pestaña Grafica 1, muestra una grafica hecha con Rbokeh, en Grafica 2 se grafica el modelo usando ggplot2, en la pestaña Tabla muestran los datos de la muestra en una tabla y en Sumario, el resumen del modelo SVM implementado
+                            
 Basada en ejemplos y la documentacion de RStudio Shiny
--- svm-master.R
+++ svm-master.R
@@ -208,11 +208,17 @@ ui <- navbarPage(theme = shinytheme("cerulean"),"SVM shiny app",
                  ),
                  tabPanel("Acerca",
                           h4("Aplicacion de SVM v 4.0"),
-                          p("Esta es una sencilla shiny app que implementa SVM sobre un conjunto de 3000 datos 
-                            creados por R y clasificados en 3 clasess. Del total de datos, especifique una submuestra, 
-                            seleccione el tipo de kernel y en base a eso configure los parametros necesarios. Se muestra 
-                            la tabla de confusion, el porcentaje correctamente clasificado segun la prediccion, 
-                            el total de vectores soporte y la grafica."),
+                          p("Esta es una sencilla shiny app que implementa SVM sobre un conjunto de datos.
+                            Por defecto, trabaja con un data frame de 3000 observaciones 
+                            creados por R y clasificados en 3 clasess. 
+                            Puede optar por la opcion de usar un archivo CSV e importarlo en la aplicacion.
+                            Del total de datos, especifique una submuestra, 
+                            seleccione el tipo de kernel y en base a eso configure los parametros necesarios. 
+                            Se muestra la tabla de confusion, el porcentaje correctamente clasificado segun la prediccion y 
+                            el total de vectores soporte.
+                            En la pestaña Grafica 1, muestra una grafica hecha con Rbokeh, en Grafica 2 se grafica el modelo 
+                            usando ggplot2, en la pestaña Tabla muestran los datos de la muestra en una tabla y en Sumario
+                            el resumen del modelo SVM implementado"),
                           p("Basada en ejemplos y la documentacion de RStudio Shiny"),
                           a(href="https://www.twitter.com/axelmora93", "Twitter"),
                           a(href="https://www.linkedin.com/in/axel-sunem-mora-olvera-338aa6108/", "LinkedIn")
